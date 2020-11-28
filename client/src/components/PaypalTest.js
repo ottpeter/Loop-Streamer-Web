e6527cc135a0cf12ac22f6486f526280553e1ae9
Péter Ott <ottpeter@tuta.io>
2020-11-28 17:49:20 +0100

    fetch instead of actions.request.post

--- client/src/components/PaypalTest.js
+++ client/src/components/PaypalTest.js
@@ -1,4 +1,4 @@
-import React, {useEffect} from 'react'
+import React, {useEffect} from 'react';;
 
 export default function PaypalComponent() {
     useEffect(()=> {
@@ -18,11 +18,22 @@ export default function PaypalComponent() {
             // Execute the payment:
             // 1. Add an onAuthorize callback
             onAuthorize: function(data, actions) {
+              console.log("data.paymentID: ", data.paymentID);
+              console.log("data.payerID: ", data.payerID);
+              const requestOptions = {
+                method: 'POST',
+                headers: { 'Content-Type': 'application/json' },
+                body: JSON.stringify({ 
+                  paymentID: data.paymentID,
+                  payerID:   data.payerID 
+                })
+              };
               // 2. Make a request to your server
-              return actions.request.post('https://63-250-57-43.cloud-xip.io:5000/paypaltest/execute-payment', {
+              return fetch('https://63-250-57-43.cloud-xip.io:5000/paypaltest/execute-payment', requestOptions)
+              /*return actions.request.post('https://63-250-57-43.cloud-xip.io:5000/paypaltest/execute-payment', {
                 paymentID: data.paymentID,
                 payerID:   data.payerID
-              })
+              })*/
                 .then(function(res) {
                   // 3. Show the buyer a confirmation message.
                   alert("payment got successful");
