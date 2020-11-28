85b2872ad666b34a11f2fd0373c1fb5cb72d7303
Péter Ott <ottpeter@tuta.io>
2020-11-28 15:57:21 +0100

    Cross origin

--- server/index.js
+++ server/index.js
@@ -12,6 +12,13 @@ const pool = require('./db');
 //test.serviceHandler("test", "nothing");
 
 // Middleware
+  //Allow Cross-Origin
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "http://localhost:3000");
+  res.header('Access-Control-Allow-Credentials', true);
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 app.use(cors());
 app.use(express.json());
 
--- server/routes/paypalTest.js
+++ server/routes/paypalTest.js
@@ -16,7 +16,7 @@ function exampleCall() {
 // Add your client ID and secret
 // These will be in env
 var CLIENT =
-  'AUJoKVGO3q1WA1tGgAKRdY6qx0qQNIQ6vl6D3k7y64T4qh5WozIQ7V3dl3iusw5BwXYg_T5FzLCRguP8';
+  'AahP0uPuabNc2rxw-3Grnsf8ncNchABTp6rPHnrEJ8FVuWw2_uoKY7b8uo-vejfPzfTyrY-0ldPJXAfQ';
 var SECRET =
   'EOw8LNwDhM7esrQ3nHfzKc7xiWnJc83Eawln4YLfUgivfx1LGzu9Mj0F5wlarilXDqdK9Q5aHVo-VGjJ';
 var PAYPAL_API = 'https://api-m.sandbox.paypal.com';
