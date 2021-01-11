04d677a7dce15b1837e6441c0df64c4be2c01cfd
Péter Ott <ottpeter@tuta.io>
2021-01-11 11:35:33 +0100

    Edits db.js

--- server/db.js
+++ server/db.js
@@ -4,11 +4,11 @@ require('dotenv').config();
 
 //Password should be stored in .env file, or something like that.s
 const pool = new Pool({
-    user: "dbadmin",
+    user: "sammy",
     password: process.env.DB_PASS,
     host: "127.0.0.1",
     port: 5432,
-    database: ""
+    database: "loopstreamer"
 });
 
 module.exports = pool;
